7c8ef78434c6896999facb7172e0f871791f3d9a
rahulgote4 <goterahul2000@gmail.com>
2025-03-30 17:33:05 +0530

    Model class files Added

--- src/main/java/com/test/nimap/models/categories.java
+++ src/main/java/com/test/nimap/models/categories.java
@@ -1,5 +1,25 @@
 package com.test.nimap.models;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity
 public class categories {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int String;
+	
+	@OneToMany(mappedBy = "categories",cascade = CascadeType.ALL)
+	private List<products>product;
+	
 
 }
--- src/main/java/com/test/nimap/models/products.java
+++ src/main/java/com/test/nimap/models/products.java
@@ -1,5 +1,33 @@
 package com.test.nimap.models;
 
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import lombok.Data;
+@Data
+@Entity
 public class products {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pid;
+	private String pname;
+	private  double price;
+	
+	@ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	@JoinColumn(name = "category_id")
+	@JsonIgnore
+	private categories categori;
 
 }
